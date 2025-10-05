761f4c0616d6f1e2ab1fcd21aaf2d1b82f118cf8
mahmoudsami778899-beep <mahmoud.sami778899@gmail.com>
2025-10-05 15:36:40 +0300

    Add files via upload

---
+++ task 1 work.py
@@ -0,0 +1,17 @@
+
+item1_price=80 , item2_price=50 , item3_price=70
+
+total_budget=200
+
+total_cost=item1_price+item2_price+item3_price
+
+print('total_cost ' , total_cost)
+print('total_budget:' , total_budget)
+
+
+if total_cost <= total_budget
+remind = total_budget - total_cost
+print('done pudget',remind  )
+
+else :needed = total_cost - total_budget
+print('needed' needed)
