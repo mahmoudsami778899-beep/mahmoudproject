bfffd3272f3deae795d8f796f56b45812ca6b5c7
mahmoudsami778899-beep <mahmoud.sami778899@gmail.com>
2025-10-19 18:46:00 +0300

    Add files via upload

---
+++ task 3.py
@@ -0,0 +1,40 @@
+
+def is_prime(n):
+    if n <= 1:
+        return False  
+    for i in range(2, int(n ** 0.5) + 1):  
+        if n % i == 0:
+            return False
+    return True
+
+
+# calculator
+def calculator(a, b, operation):
+    if operation == "add":
+        return a + b
+    elif operation == "subtract":
+        return a - b
+    elif operation == "multiply":
+        return a * b
+    elif operation == "divide":
+        if b == 0:
+            return "Error: Division by zero!"
+        return a / b
+    else:
+        return "Invalid operation!"
+
+
+
+number = int(input("Enter a number to check if it's prime: "))
+if is_prime(number):
+    print(f"{number} is a prime number.")
+else:
+    print(f"{number} is not a prime number.")
+
+
+a = float(input("Enter first number: "))
+b = float(input("Enter second number: "))
+operation = input("Enter operation (add, subtract, multiply, divide): ")
+
+result = calculator(a, b, operation)
+print("Result:", result)
